8ee2a2122bb2eece7d09f6931cb2254b0138258a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-04 23:03:06 -0700

    coded a method which returns a smallest path when the characters of the string changes and form endWord from beginWord

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -13,7 +13,40 @@ class Pair{
 
 public class WordLadder {
 
-    public static int wordLadder(String beginWord, String endWord, List<String> wordList)
+    public static int wordLadder(String beginWord, String endWord, List<String> wordList){
+        HashSet<String> hs = new HashSet<>(wordList);
+        hs.remove(beginWord);
+
+        Queue<Pair> q = new LinkedList<>();
+        Pair pair = new Pair(beginWord, 1);
+        q.add(pair);
+
+        while(!q.isEmpty()){
+            Pair pp = q.remove();
+            int level = pp.level;
+            String current = pp.currWord;
+
+            if(current ==endWord){
+                return level;
+            }
+            for(int i=0; i<current.length(); i++){
+
+                char [] charArray = current.toCharArray();
+                for(char ch ='a'; ch<= 'z'; ch++){
+                    charArray[i]=ch;
+
+                    String temp =new String(charArray);
+
+                    if(hs.contains(temp)){
+                        hs.remove(temp);
+                        Pair p1 = new Pair(temp, level+1);
+                        q.add(p1);
+                    }
+                }
+            }
+
+        }return 0;
+    }
 
 
 
