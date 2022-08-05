fbc161c098942065119da3887d1da9ffcb537ed6
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-04 22:55:28 -0700

    coded a pair class to keep the track of the word and its level itself

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
+class Pair{
+    int level;
+    String currWord;
+    Pair(String currWord, int level){
+        this.currWord =currWord;
+        this.level = level;
+    }
+}
+
+
+
 public class WordLadder {
-    
+
+    public static int wordLadder(String beginWord, String endWord, List<String> wordList)
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
